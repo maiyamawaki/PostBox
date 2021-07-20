c5a1a98c4b24c0d9a1b49a901ce3122e3fd68457
Mai Yamawaki <joooooy031006@icloud.com>
2021-07-20 21:02:42 +0900

    proyecto iniciado

---
+++ PostBox.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+
+public class PostBox {
+	public static void main(String[] args) {
+		PostBox app = new PostBox();
+		app.start();
+	}
+
+	public void start() {
+		
+	}
+}
