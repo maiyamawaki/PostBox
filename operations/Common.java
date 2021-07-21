e086f865cc9519e78eaf46b92302b5340f1f5f58
Mai Yamawaki <joooooy031006@icloud.com>
2021-07-21 23:51:53 +0900

    csv functions added

--- operations/Common.java
+++ operations/Common.java
@@ -8,6 +8,34 @@ import java.util.HashMap;
 
 public class Common {
 	public HashMap<Integer, Post> getMapFromFile() {
-		
+		HashMap<Integer, Post> map = new HashMap<Integer, Post>();
+		try(BufferedReader input = new BufferedReader(new PrintReader("data.csv"))) {
+			String data = "";
+			while((data = input.readLine())!=null) {
+				int id = Integer.parseInt(data.split(",")[0]);
+				Stirng title = data.split(",")[1];
+				String contents = data.split(",")[2];
+				map.put(id, new Post(title, contents));
+			}
+		} catch(IOException e) {
+			System.out.println("Ocurrió error al abrir el csv.");
+		}
+		return map;
+	}
+
+	public void updateFile(HashMap<Integer, Post> map) {
+		try(BufferedWriter output = new BufferedWriter(new PrintWriter("data.csv"))) {
+			for (int key : map.keySet()) {
+				Post post = map.get(key);
+				output.write(String.valueOf(key));
+				output.write(",");
+				output.write(post.getTitle());
+				output.write(",");
+				output.write(post.getContents());
+				output.newLine();
+			}
+		}catch(IOException e) {
+			System.out.println("Occurió error al abrir el csv.");
+		}
 	}
 }
